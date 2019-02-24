012691648b35edc9201d3e40abe6f8796e163932
Ihor Horash <gorashigor@gmail.com>
2019-02-24 12:34:59 +0200

    added cell hover event and statusbar

--- js/main.js
+++ js/main.js
@@ -1,42 +1,86 @@
 require([
     'dojo/dom',
     'dojo/on',
+    'dojo/query',
+    'dojo/dom-class',
     'dojo/_base/fx',
     'dojo/dom-style',
     'dojo/mouse',
     'dojo/domReady!'
-], function (dom, on, fx, domStyle, mouse) {
+], function (dom, on, query, domClass, fx, domStyle, mouse) {
 
     var player1Name = "";
     var player2Name = "";
+    var player1Symbol = "X";
+    var player2Symbol = "O";
+    var changePlayer = false;
+    var session = sessionStorage;
 
     var player1InputDiv = dom.byId('player1InputDiv');
     var player1Input = dom.byId('player1Input');
     var player1Text = dom.byId('player1Text');
     var player1Button = dom.byId('player1Button');
+    var cells = query('div.grid-cell');
+    var grid = dom.byId('grid');
 
-    on(player1Input, 'change', function(e) {
+    console.log('ready');
+    session.clear();
+
+    if(!session.getItem('gameStarted')){
+        session.setItem('gameStarted', 'started');
+    }    
+
+    var showGrid = function(){
+        if(player1Name.length > 0 && player2Name.length > 0){
+            domClass.remove('grid', 'hidden');
+        }
+    }
+
+    on(player1Input, 'change', function(e){
         player1Name = e.target.value;
     });
-    on(player1Button, 'click', function(e) {
+    on(player1Button, 'click', function(e){
         if(player1Name.length === 0) {
             player1Name = player1Input.placeholder;            
         }
         player1Text.innerHTML = player1Name;
+        session.setItem(player1Text.id, player1Name);
         fx.fadeOut({ node: player1InputDiv}).play();
-        fx.fadeIn({ node: player1Text}).play();
+        showGrid();
     });
 
-    on(player2Input, 'change', function(e) {
+    on(player2Input, 'change', function(e){
         player2Name = e.target.value;
     });
-    on(player2Button, 'click', function(e) {
+    on(player2Button, 'click', function(e){
         if(player2Name.length === 0) {
             player2Name = player2Input.placeholder;            
         }
         player2Text.innerHTML = player2Name;
+        session.setItem(player2Text.id, player2Name);
         fx.fadeOut({ node: player2InputDiv}).play();
-        fx.fadeIn({ node: player2Text}).play();
+        showGrid();
+    });    
+
+    cells.on('click', function(e){
+        if(e.target.innerHTML === "") {
+            if(!changePlayer) {
+                e.target.innerHTML = player1Symbol;
+            } else {
+                e.target.innerHTML = player2Symbol;
+            }
+            changePlayer = !changePlayer;
+        }        
     });
+
+    cells.on(mouse.enter, function(e){
+        domClass.add(e.target.id, 'grid-cell-hover');      
+    });
+
+    cells.on(mouse.leave, function(e){
+        domClass.remove(e.target.id, 'grid-cell-hover');        
+    });
+
+
 
 });
